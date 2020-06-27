34496f7e8bedadc7426102d8428da3981832af55
Nazar Ashirov <newnewqwes@gmail.com>
2020-06-27 14:46:36 +0300

    subm

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,96 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MaterialTable, { Column } from 'material-table';
 
-function App() {
+
+
+interface Row {
+  nameRU: string;
+  nameEN: string;
+  nationalIdNumber: number;
+  contractStartDate: any;
+}
+
+interface TableState {
+  columns: Array<Column<Row>>;
+  data: Row[];
+}
+
+/*
+ React.FC обозначает явный тип для этой функции (хранится в React FC = function component) она проверяет возвращается ли jsx разметка
+*/
+const App: React.FC = () => {
+  const [state, setState] = React.useState<TableState>({
+    columns: [
+      { title: 'Name in Russian', field: 'nameRU' },
+      { title: 'Name in English', field: 'nameEN' },
+      { title: 'National ID Number', field: 'nationalIdNumber', type: 'numeric' },
+      {
+        title: 'Contract Start Date',
+        field: 'contractStartDate',
+        type: ('date')
+      },
+    ],
+    data: [
+      { nameRU: 'Дарья', nameEN: 'Darya', nationalIdNumber: 86756453, contractStartDate: '15.06.2020' },
+      {
+        nameRU: 'Назар',
+        nameEN: 'Nazar',
+        nationalIdNumber: 103032,
+        contractStartDate: '26.06.2020',
+      },
+    ],
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MaterialTable
+      title="HR System v2.0"
+      columns={state.columns}
+      data={state.data}
+      editable={{
+        onRowAdd: (newData) =>
+          new Promise((resolve) => {
+            setTimeout(() => {
+              resolve();
+              setState((prevState) => {
+                const data = [...prevState.data];
+                data.push(newData);
+                return { ...prevState, data };
+              });
+            }, 600);
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise((resolve) => {
+            setTimeout(() => {
+              resolve();
+              if (oldData) {
+                setState((prevState) => {
+                  const data = [...prevState.data];
+                  data[data.indexOf(oldData)] = newData;
+                  return { ...prevState, data };
+                });
+              }
+            }, 600);
+          }),
+        onRowDelete: (oldData) =>
+          new Promise((resolve) => {
+            setTimeout(() => {
+              resolve();
+              setState((prevState) => {
+                const data = [...prevState.data];
+                data.splice(data.indexOf(oldData), 1);
+                return { ...prevState, data };
+              });
+            }, 600);
+          }),
+      }}
+    />
   );
+
 }
 
 export default App;
+
+
+//______________________
+
+ 
